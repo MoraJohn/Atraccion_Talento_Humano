848c4cf6de31ac4efa95bfe24d8e59f11069422d
John <johnsebasm@gmail.com>
2023-01-11 12:27:51 -0600

    johnBranch añadida

---
+++ AtraccionTalentoHumanoWebApp/Controllers/AccesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AtraccionTalentoHumanoWebApp.Controllers
+{
+    public class AccesController : Controller
+    {
+        // GET: Acces
+        public ActionResult AccesIndex()
+        {
+            return View();
+        }
+    }
+}
